2992d5588cd8b0fdbc1fba0846ee21ebbe54e555
Luis Penaloza <ldp231@yahoo.com>
2020-09-22 21:55:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), final_array|
     #binding.pry 
     value.each do |inner_key, names|
-      name.each do 
+      name.each do |name|
         #binding.pry
         if !final_array[name]
           final_array = {}
